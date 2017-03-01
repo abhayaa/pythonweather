61209cbdcaca931ae3b722dcf687a55f65af71f8
Abhaya Basnet <basnetabhaya@gmail.com>
2017-02-28 20:45:01 -0600

    Add files via upload

--- weatherBot.py
+++ weatherBot.py
@@ -20,27 +20,59 @@ def temperature():
 
     if userInput.isdigit() == True:
         response= requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+userInput+',us&appid=fe22db5569ab3626316cda1a75a095a8')
+
         json_object = response.json()
-        city = str(json_object['name'])
-        condition = str(json_object['weather'][0]['main'])
-        if condition == "shower rain" or "Snow":
-            iconCondition = "/static/styles/cloudyicon.png"
-        elif condition == "few clouds" or "scattered clouds" or "Clouds":
-            iconCondition ="/static/styles/cloudyicon.png"
+
+        city = str.lower(json_object['name'])
+
+        condition = str.lower(json_object['weather'][0]['main'])
+
+        if condition == "clear":
+            iconCondition = "cloudy1.png"
+        if condition == "few clouds":
+            iconCondition = "cloudy3.png"
+        if condition == "shower rain":
+            iconCondition = "shower3.png"
+        if condition == "rain":
+            iconCondition = "shower3.png"
+        if condition == "thunderstorm":
+            iconConditio = "tstorm2.png"
+        if condition == "snow":
+            iconCondition = "snow3.png"
+        if condition == "mist":
+            iconCondition = "mist.png"
         kelvin = float(json_object['main']['temp'])
         fahrenheit = round((kelvin - 273.15) * 1.8 + 32)
 
     elif userInput.isdigit() == False:
         response = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+userInput+',us&appid=cbccdccb44cb0e84dfa9c6d9444a209a')
+
         json_object = response.json()
+
         city = str.lower(json_object['name'])
+
         condition = str.lower(json_object['weather'][0]['main'])
-        if condition == "Snow" or "shower rain":
-            iconCondtion = "/static/styles/rainicon.png"
-        elif condition == "Clouds":
-            iconCondition = "/static/styles/cloudyicon.png"
+
+
+        if condition == "clear":
+            iconCondition = "cloudy1.png"
+        if condition == "few clouds":
+            iconCondition = "cloudy3.png"
+        if condition == "shower rain":
+            iconCondition = "shower3.png"
+        if condition == "rain":
+            iconCondition = "shower3.png"
+        if condition == "thunderstorm":
+            iconCondition = "tstorm2.png"
+        if condition == "snow":
+            iconCondition = "snow3.png"
+        if condition == "mist":
+            iconCondition = "mist.png"
+
         kelvin = float(json_object['main']['temp'])
+
         fahrenheit = round((kelvin - 273.15) * 1.8 + 32)
+
     return render_template('temperature.html', temp=fahrenheit,city=city,cond=condition,icon=iconCondition)
 
 @app.route('/')
